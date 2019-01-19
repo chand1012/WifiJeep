b3d208178003781e13e7c92bb95ad75b443f6950
Chandler Lofland <chandlerl14@outlook.com>
2019-01-18 20:55:07 -0500

    more byte errors

--- PythonTests/servertranslator.py
+++ PythonTests/servertranslator.py
@@ -32,7 +32,7 @@ while True:
             pass
         else:
             print('Command sent: %s' % command)
-            bytecmd = b'%b' % command
+            bytecmd = command.encode()
             arduino.write(bytecmd)
     client.close()
 
