62990c8a07c9f0a692e31d01bd738d5be00330fa
Chandler Lofland <chandlerl14@outlook.com>
2019-01-19 13:46:05 -0500

    trying something new

--- python/flask/app.py
+++ python/flask/app.py
@@ -13,15 +13,10 @@ codes = {
     "left":b'5'
 }
 
-@app.route("/")
+@app.route("/", methods = ['POST', 'GET'])
 def index():
     global arduino 
     arduino = serial.Serial('/dev/ttyACM0', 9600)
-    return render_template("index.html")
-
-@app.route("/postrequest", methods = ['POST'])
-def worker():
-    #moved forward
     data = request.get_json(force=True)
     sendbyte = None
     print(str(data))
@@ -30,6 +25,8 @@ def worker():
             print("Sending %s" % str(thing["byte"]))
             sendbyte = str(thing["byte"]).encode()
     arduino.write(sendbyte)
+
+    return render_template("index.html")
 
 if __name__=="__main__":
     app.run('0.0.0.0', "1166", debug=True)
