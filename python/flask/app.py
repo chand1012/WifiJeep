f50a16d5cadd202a52748cdd2d4627706217088d
Chandler Lofland <chandlerl14@outlook.com>
2019-01-19 11:00:41 -0500

    added flask code

---
+++ python/flask/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, Response
+import sys, json, random
+from time import time, sleep
+import serial
+
+arduino = serial.Serial('/dev/ttyACM0', 9600)
+arduino.write(b'0')
+app = Flask(__name__)
+codes = {
+    "straight":b'0',
+    "coast":b'1',
+    "forward":b'2',
+    "backward":b'3',
+    "right":b'4',
+    "left":b'5'
+}
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/postrequest", methods = ['POST'])
+def worker():
+    #moved forward
+    data = request.get_json()
+    print("Sending %s" % str(data["byte"]))
+    sendbyte = str(data["byte"]).encode()
+    arduino.write(sendbyte)
+
+if __name__=="__main__":
+    app.run('0.0.0.0', "1166")
