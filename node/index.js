d1d11bc7c50dee07f06f4209b8b64141199f497c
Chandler Lofland <chandlerl14@outlook.com>
2019-01-19 10:03:15 -0500

    removed node because I am bad with JS

--- node/index.js
+++
@@ -1,3 +0,0 @@
-// https://codeburst.io/build-a-weather-website-in-30-minutes-with-node-js-express-openweather-a317f904897b
-// https://stackoverflow.com/questions/4416505/how-to-take-keyboard-input-in-javascript
-// https://jacobsalmela.com/2014/05/31/raspberry-pi-webcam-using-mjpg-streamer-over-internet/
