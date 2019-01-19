b958b028f6de05d325b3c2c45a1e85c0e9dd7efa
Chandler Lofland <chandlerl14@outlook.com>
2019-01-18 21:17:27 -0500

    got main code working

--- arduino/Main/Main.ino
+++ arduino/Main/Main.ino
@@ -1,30 +1,34 @@
 // this is the primary code to run that accepts input from the Rasperry Pi server serial
 // the code constantly waits for an input so that it can be proccessed 
 // integer codes as follow, must be converted to bytes by python
-#define FORW 3 // can't use FORWARD as its a command in the adafruit
-#define BACK 2 // motor library
+#define FORW 2 // can't use FORWARD as its a command in the adafruit
+#define BACK 3 // motor library
 #define COAST 1
 
 //these can be used at the same time as a the previous ones
-#define RIGHT 5
-#define LEFT 4
+#define RIGHT 4
+#define LEFT 5
 #define STRAIGHT 0
 
-#define STEER 0 //these are subject to change
-#define DRIVE 1 //they define the motor number on the motor shield
+#define STEER 1 //these are subject to change
+#define DRIVE 3 //they define the motor number on the motor shield
+
+#define POWAH 255
 
 #include <AFMotor.h>
 
 AF_DCMotor drive(DRIVE); 
 AF_DCMotor steer(STEER);
 
+unsigned int steerState = STRAIGHT; //states will hold unless otherwise told
+unsigned int driveState = COAST;
+unsigned int recv;
+unsigned int n;
+
 void setup() {
     Serial.begin(9600);
-    drive.setSpeed(255);
-    steer.setSpeed(255);
-    unsigned int steerState = STRAIGHT; //states will hold unless otherwise told
-    unsigned int driveState = COAST;
-    unsigned int recv;
+    drive.setSpeed(POWAH);
+    steer.setSpeed(POWAH);
     Serial.write("Initialized.");
 }
 
@@ -32,7 +36,7 @@ void loop() {
     if(Serial.available()){ //read the serial
         n = Serial.read() - '0';
     }
-
+     
     if (n==STRAIGHT){ //interpret the numbers
         steer.run(RELEASE);
     } else if (n==COAST) {
@@ -40,10 +44,11 @@ void loop() {
     } else if (n==BACK) {
         drive.run(BACKWARD);
     } else if (n==FORW) {
-        steer.run(FORWARD)
+        steer.run(FORWARD);
     } else if (n==LEFT) {
-        steer.run(BACKWARD)
+        steer.run(BACKWARD);
     } else if (n==RIGHT) {
-        steer.run(FORWARD)
+        steer.run(FORWARD);
     } 
 }
+// Tested: Yay it works!
