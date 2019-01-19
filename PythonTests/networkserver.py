5dad5304416f6e7b17b5ed830e00b4bb6f0f8f62
Chandler Lofland <chandlerl14@outlook.com>
2019-01-18 19:13:11 -0500

    added arduino pyserial code to server

--- PythonTests/networkserver.py
+++ PythonTests/networkserver.py
@@ -1,7 +1,9 @@
 import socket
 import sys
 import signal
+import serial
 
+arduino = serial.Serial('/dev/ttyACM0', 9600) #this will change if running this on windows but this is for linux
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
@@ -26,8 +28,10 @@ while True:
             pass
         elif command=='on':
             print("Turning on")
+            arduino.write(b'1')
         elif command=='off':
             print("Turning off")
+            arduino.write(b'0')
         elif command=='stop':
             break            
         elif command=='exit' or int(command)==-1:
