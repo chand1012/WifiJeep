b01f61f2770fff30393c36c397a7dd0b61b481c8
Chandler Lofland <chandlerl14@outlook.com>
2019-01-19 13:33:38 -0500

    removed issue

--- python/flask/app2.py
+++ python/flask/app2.py
@@ -11,7 +11,6 @@ codes = {
     "right":b'4',
     "left":b'5'
 }
-value = 'coast'
 app = Flask(__name__)
 @app.route("/", methods = ['POST', 'GET'])
 def index(value):
@@ -20,7 +19,7 @@ def index(value):
         value = request.form['submit'].lower()
         arduino.write(codes[value])
 
-    return render_template('index2.html', value=str(value))
+    return render_template('index2.html')
 
 if __name__=="__main__":
     app.run('0.0.0.0', "1166", debug=True)
