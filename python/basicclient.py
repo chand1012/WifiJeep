c5573bfb9ef18e2b1ac1b774dbcb2714baa87936
Chandler Lofland <chandlerl14@outlook.com>
2019-01-18 22:52:16 -0500

    added main python files

---
+++ python/basicclient.py
@@ -0,0 +1,44 @@
+import pygame, sys
+import socket
+
+host = sys.argv[1]
+port = None
+try:
+    port = int(sys.argv[2])
+except IndexError:
+    port = 1166
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+print("Starting client to {} on port {}...".format(host, port))
+s.connect((host, port))
+print("Connected!")
+n = 0
+while True:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        sys.exit(0)
+                        s.close()
+        
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_UP]: 
+            n = 2
+        elif pressed[pygame.K_DOWN]: 
+            n = 3
+        else:
+            n = 1
+
+        if pressed[pygame.K_LEFT]: 
+            n = 5
+        elif pressed[pygame.K_RIGHT]: 
+            n = 4
+        else:
+            n = 0 
+
+        if pressed[pygame.K_ESCAPE]:
+            pygame.quit()
+            
+        s.send(str(n).encode('ascii'))
+        
+        
---
+++ python/piserver.py
@@ -0,0 +1,42 @@
+import socket
+import sys
+import signal
+import serial
+
+# literally just takes whatever it receives 
+# and forwards it to the arduino
+
+arduino = serial.Serial('/dev/ttyACM0', 9600) #this will change if running this on windows but this is for linux
+arduino.write(b'0')
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+host = sys.argv[1]
+port = None
+try:
+    port = int(sys.argv[2])
+except IndexError:
+    port = 1166
+
+server.bind((host, port))
+
+server.listen(5)
+print("Starting server")
+while True:
+    client, addr = server.accept()
+
+    print("Connection from %s" % str(addr))
+
+    while True:
+        cmd = client.recv(1024)
+        command = cmd.decode('ascii').strip()
+        # gets command from client and processes it
+        if command=='':
+            pass
+        else:
+            print('Command sent: %s' % command)
+            bytecmd = command.encode()
+            arduino.write(bytecmd)
+    client.close()
+
+        
