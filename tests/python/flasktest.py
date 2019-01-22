33132556ee843d1f3dd39936aa46b7a594ffc145
chand1012 <chandlerl14@outlook.com>
2019-01-22 17:53:01 -0500

    need to get the video streaming working

--- tests/python/flasktest.py
+++ tests/python/flasktest.py
@@ -1,24 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response, jsonify
 import serial, sys
+from webcamera import VideoCamera
 
 app = Flask(__name__)
-codes = {
-    "straight":b'0',
-    "coast":b'1',
-    "forward":b'2',
-    "backward":b'3',
-    "right":b'4',
-    "left":b'5'
-}
 
-'''
-arduino = None
+video_camera = None
+global_frame = None
 
-if sys.platform=="win32":
-    arduino = serial.Serial('COM4', 9600)
-else:
-    arduino = serial.Serial('/dev/ttyACM0', 9600)
-'''
+def video_stream(): # this is whats breaking it
+    
+    if video_camera == None:
+        video_camera = VideoCamera()
+    
+    while True:
+        frame = video_camera.get_frame()
+
+        if frame != None:
+            global_frame = frame
+            yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        else:
+            yield (b'--frame\r\n'
+                            b'Content-Type: image/jpeg\r\n\r\n' + global_frame + b'\r\n\r\n')
+
+
 @app.route("/")
 def index():
     return render_template("index.test.html")
@@ -29,6 +34,26 @@ def worker():
     #arduino.write(codes[data])
     print("POST request recieved: sent %s" % data)
     return data
+
+def record_status():
+    global video_camera 
+    if video_camera == None:
+        video_camera = VideoCamera()
+
+    json = request.get_json()
+
+    status = json['status']
+
+    if status == "true":
+        video_camera.start_record()
+        return jsonify(result="started")
+    else:
+        video_camera.stop_record()
+        return jsonify(result="stopped")
+
+@app.route("/record")
+def video_viewer():
+    return Response(video_stream(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__=="__main__":
     app.run('0.0.0.0', "1166", debug=True)
